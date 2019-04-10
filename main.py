3388eef5454a5b109f21b0c7c6564c610a4b9d20
Björn Guth <bjoern.guth@rwth-aachen.de>
2019-04-10 23:39:46 +0200

    Implement theater selectors for new lists

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.dropdown import DropDown
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.popup import Popup
@@ -16,7 +18,7 @@ import random
 import os
 
 
-nationalities = [
+nations = [
     "Britain",
     "France",
     "Germany",
@@ -26,24 +28,29 @@ nationalities = [
     "United States",
 ]
 
+theater_selector_dict = {
+    n: ["{} - {}".format(n, i) for i in range(20)] for n in nations
+}
+
 default_list_dict = {
     "lists": {
         "default": {
             "hq": {"major": None, "captain": None, "infantry": None},
             "platoons": [],
             "nationality": "Britain",
+            "theater_selector": "1944 - Normandy",
         }
     },
     "session_data": {"current_list": "default"},
 }
 
 
-class StartGrid(GridLayout):
+class ManagerLayout(BoxLayout):
 
     army_list = DictProperty()
 
     def __init__(self, **kwargs):
-        super(StartGrid, self).__init__(**kwargs)
+        super(ManagerLayout, self).__init__(**kwargs)
 
         self.army_list_data = JsonStore(
             os.path.join(App.get_running_app().user_data_dir, "army_list.json"),
@@ -57,13 +64,33 @@ class StartGrid(GridLayout):
             self.army_list["lists"] = default_list_dict["lists"]
             self.army_list["session_data"] = default_list_dict["session_data"]
 
-        self.ids["label_current_list"].text = self.army_list["session_data"][
-            "current_list"
-        ]
+        start_grid = StartGrid(self.army_list)
+        self.add_widget(start_grid)
 
     def on_army_list(self, instance, army_list):
         for key, value in army_list.items():
             self.army_list_data[key] = value
+
+    def switch_to_start(self, instance):
+        self.remove_widget(instance)
+        start_grid = StartGrid(self.army_list)
+        self.add_widget(start_grid)
+
+    def switch_to_game(self, instance):
+        self.remove_widget(instance)
+        game_grid = GameGrid(self.army_list)
+        self.add_widget(game_grid)
+
+
+class StartGrid(GridLayout):
+    def __init__(self, army_list, **kwargs):
+        super(StartGrid, self).__init__(**kwargs)
+
+        self.army_list = army_list
+
+        self.ids["label_current_list"].text = self.army_list["session_data"][
+            "current_list"
+        ]
 
     def select_army_list(self):
         popup = ArmySelectPopup(self.army_list)
@@ -72,17 +99,28 @@ class StartGrid(GridLayout):
 
     def update_current_army_list(self, instance):
         if instance.selected_list and (
-            self.army_list_data["session_data"]["current_list"]
-            != instance.selected_list
+            self.army_list["session_data"]["current_list"] != instance.selected_list
         ):
             self.ids["label_current_list"].text = instance.selected_list
-            session_data = self.army_list_data["session_data"]
+            session_data = self.army_list["session_data"]
             session_data["current_list"] = instance.selected_list
-            self.army_list_data["session_data"] = session_data
+            self.army_list["session_data"] = session_data
 
     def delete_army_lists(self):
         popup = ArmyListDeletePopup(self.army_list)
         popup.open()
+
+    def start_game(self):
+        self.parent.switch_to_game(self)
+
+
+class GameGrid(GridLayout):
+    def __init__(self, army_list, **kwargs):
+        super(GameGrid, self).__init__(**kwargs)
+        self.army_list = army_list
+
+    def switch(self):
+        self.parent.switch_to_start(self)
 
 
 class ArmySelectPopup(Popup):
@@ -99,8 +137,7 @@ class ArmySelectPopup(Popup):
         popup.open()
 
     def add_new_army_list(self, instance):
-        # TODO: don't add list name to self.army_listing when creation canceled
-        if instance.ids["textinput_name"].text:
+        if instance.list_name:
             self.army_listing.add_data_item(instance.ids["textinput_name"].text)
 
     def ok(self):
@@ -117,15 +154,40 @@ class NewArmyListPopup(Popup):
     def __init__(self, army_list, **kwargs):
         super(NewArmyListPopup, self).__init__(**kwargs)
         self.army_list = army_list
-        self.nation_selection = SelectableListing(nationalities)
-        self.ids["layout_nation_selection"].add_widget(self.nation_selection)
+        self.list_name = None
+        self.nation_dropdown = DropDown()
+        for nation in theater_selector_dict.keys():
+            button = DropDownButton(text=nation)
+            button.bind(on_release=lambda btn: self.nation_dropdown.select(btn.text))
+            self.nation_dropdown.add_widget(button)
+        self.ids["button_nation_selection"].bind(on_release=self.nation_dropdown.open)
+        self.nation_dropdown.bind(on_select=self.set_nationality)
+        self.theater_dropdown = DropDown()
+        self.ids["button_theater_selector"].bind(on_release=self.theater_dropdown.open)
+        self.theater_dropdown.bind(on_select=self.set_theater_selector)
+
+    def set_nationality(self, instance, nationality):
+        self.theater_dropdown.dismiss()
+        if nationality != self.ids["button_nation_selection"].text:
+            self.ids["button_nation_selection"].text = nationality
+            self.ids["button_theater_selector"].text = "select"
+            self.theater_dropdown.clear_widgets()
+            for selector in theater_selector_dict[nationality]:
+                button = DropDownButton(text=selector)
+                button.bind(
+                    on_release=lambda btn: self.theater_dropdown.select(btn.text)
+                )
+                self.theater_dropdown.add_widget(button)
+
+    def set_theater_selector(self, instance, theater_selector):
+        self.ids["button_theater_selector"].text = theater_selector
 
     def cancel(self):
         self.dismiss()
 
     def ok(self):
         if self.ids["textinput_name"].text == "":
-            self.name = None
+            MessagePopup("List With No Name", "Please give your new list a name")
         elif self.ids["textinput_name"].text in self.army_list["lists"].keys():
             MessagePopup(
                 "List already exist",
@@ -133,16 +195,28 @@ class NewArmyListPopup(Popup):
                     self.ids["textinput_name"].text
                 ),
             )
+        elif self.ids["button_nation_selection"].text == "select":
+            MessagePopup("No nation selected", "Please select a nation.")
+        elif self.ids["button_theater_selector"].text == "":
+            MessagePopup(
+                "No theater selector selected", "Please select a theater selector"
+            )
         else:
-            nation = self.nation_selection.get_selection()
-            if len(nation) == 1:
-                self.name = self.ids["textinput_name"].text
-                lists_dict = self.army_list["lists"]
-                lists_dict[self.name] = default_list_dict['lists']
-                self.army_list["lists"] = lists_dict
-                self.dismiss()
-            else:
-                MessagePopup("No nation selected", "Please select a nation.")
+            self.list_name = self.ids["textinput_name"].text
+            lists_dict = self.army_list["lists"]
+            lists_dict[self.list_name] = default_list_dict["lists"]["default"]
+            lists_dict[self.list_name]["nationality"] = self.ids[
+                "button_nation_selection"
+            ].text
+            lists_dict[self.list_name]["theater_selector"] = self.ids[
+                "button_theater_selector"
+            ].text
+            self.army_list["lists"] = lists_dict
+            self.dismiss()
+
+
+class DropDownButton(Button):
+    pass
 
 
 class ArmyListDeletePopup(Popup):
@@ -258,7 +332,7 @@ class SelectableListing(RecycleView):
 
 class CompanyCommanderApp(App):
     def build(self):
-        return StartGrid()
+        return ManagerLayout()
 
 
 if __name__ == "__main__":
